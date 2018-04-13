da22e5b8d8019308b8adf60ff6939d5ae656b6dc
Matthew Xi <matthew.fullstackgis@gmail.com>
2018-04-13 14:47:46 -0700

    Hacked in a blurriness detection algorithm

--- app/src/main/java/org/greenstand/android/TreeTracker/activities/CameraActivity.java
+++ app/src/main/java/org/greenstand/android/TreeTracker/activities/CameraActivity.java
@@ -2,10 +2,12 @@ package org.greenstand.android.TreeTracker.activities;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.graphics.Matrix;
 import android.hardware.Camera;
 import android.hardware.Camera.PictureCallback;
@@ -17,7 +19,9 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.FileProvider;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
+import android.view.ContextThemeWrapper;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.FrameLayout;
@@ -127,21 +131,100 @@ public class CameraActivity extends Activity implements PictureCallback, OnClick
 			Log.e("file not", "created");
 			e.printStackTrace();
 		}
-		
+
 		try {
 			FileOutputStream fo = new FileOutputStream(tmpImageFile);
-		    	fo.write(data);
-		    	fo.close();
+            fo.write(data);
+            fo.close();
 		}
 		catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		setPic();
+
+        checkBlurriness();
+/*
+        setPic();
 		safeToTakePicture = true;
 		savePicture();      //skip picture preview
 		releaseCamera();
+		*/
 	}
+
+	private void checkBlurriness(){
+        Bitmap bitmap = openImage();
+        if(bitmap == null){
+            return;
+        }
+
+        // Calculate Sharpness - experimental
+        // https://stackoverflow.com/questions/21107560/simple-way-to-check-if-an-image-bitmap-is-blur
+        double n1sumR = 0;
+        double n2sumR = 0;
+        double n1sumG = 0;
+        double n2sumG = 0;
+        double n1sumB = 0;
+        double n2sumB = 0;
+        for(int y=1;y<bitmap.getHeight();y++) {
+            for (int x = 1; x < bitmap.getWidth(); x++) {
+                int pixel0 = bitmap.getPixel(x,y);
+                int pixel1 = bitmap.getPixel(x-1,y);
+                int pixel2 = bitmap.getPixel(x,y-1);
+
+                int A0 = (pixel0 >> 24) & 0xff; // or color >>> 24
+                int R0 = (pixel0 >> 16) & 0xff;
+                int G0 = (pixel0 >>  8) & 0xff;
+                int B0 = (pixel0      ) & 0xff;
+
+                int A1 = (pixel1 >> 24) & 0xff; // or color >>> 24
+                int R1 = (pixel1 >> 16) & 0xff;
+                int G1 = (pixel1 >>  8) & 0xff;
+                int B1 = (pixel1      ) & 0xff;
+
+                int A2 = (pixel2 >> 24) & 0xff; // or color >>> 24
+                int R2 = (pixel2 >> 16) & 0xff;
+                int G2 = (pixel2 >>  8) & 0xff;
+                int B2 = (pixel2      ) & 0xff;
+
+
+                n1sumR= n1sumR + Math.abs(R0-A1) + Math.abs(R0-R2);
+                n2sumR = n2sumR + (double) ( (R0-R1)^2 + (R0-R2)^2 );
+
+                n1sumG= n1sumG + Math.abs(G0-G1) + Math.abs(G0-G2);
+                n2sumG = n2sumG + (double) ( (G0-G1)^2 + (G0-G2)^2 );
+
+                n1sumB= n1sumB + Math.abs(B0-B1) + Math.abs(B0-B2);
+                n2sumB = n2sumB + (double)  ( (B0-B1)^2 + (B0-B2)^2 );
+
+            }
+        }
+
+        double N_pixel = bitmap.getHeight() * bitmap.getWidth();
+        double n1R = n1sumR / (2 * N_pixel);
+        double n2R = n2sumR / (2 * N_pixel);
+        double blurinessR = n2R / (n1R*n1R);
+        double n1G = n1sumG / (2 * N_pixel);
+        double n2G = n2sumG / (2 * N_pixel);
+        double blurinessG = n2G / (n1G*n1G);
+        double n1B = n1sumB / (2 * N_pixel);
+        double n2B = n2sumB / (2 * N_pixel);
+        double blurinessB = n2B / (n1B*n1B);
+
+        double blurinessMean = (blurinessR + blurinessG + blurinessB) /3 ;
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(new ContextThemeWrapper(this, R.style.myDialog));
+        builder.setTitle("Blurriness Calculated")
+                .setMessage(String.valueOf(blurinessMean))
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        setPic();
+                        safeToTakePicture = true;
+                        savePicture();      //skip picture preview
+                        releaseCamera();                    }
+                })
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+    }
 	
 	private void galleryAddPic() throws IOException {
 		Intent mediaScanIntent = new Intent(
@@ -149,8 +232,8 @@ public class CameraActivity extends Activity implements PictureCallback, OnClick
 		
 		Bitmap photo = Utils.resizedImage(mCurrentPhotoPath);
 		
-       		ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-        	photo.compress(Bitmap.CompressFormat.JPEG, 70, bytes);
+		ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+		photo.compress(Bitmap.CompressFormat.JPEG, 70, bytes);
 
 		File f = new File(mCurrentPhotoPath);
 		try {
@@ -262,39 +345,43 @@ public class CameraActivity extends Activity implements PictureCallback, OnClick
 		return f;
 	}
 
-	private void setPic() {
-
-		/* There isn't enough memory to open up more than a couple camera photos */
+	private Bitmap openImage(){
+	    /* There isn't enough memory to open up more than a couple camera photos */
 		/* So pre-scale the target bitmap into which the file is decoded */
 
 		/* Get the size of the image */
-		BitmapFactory.Options bmOptions = new BitmapFactory.Options();
-		bmOptions.inJustDecodeBounds = true;
-		BitmapFactory.decodeFile(tmpImageFile.getAbsolutePath(), bmOptions);
-		int imageWidth = bmOptions.outWidth;
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(tmpImageFile.getAbsolutePath(), bmOptions);
+        int imageWidth = bmOptions.outWidth;
 
-		// Calculate your sampleSize based on the requiredWidth and
-		// originalWidth
-		// For e.g you want the width to stay consistent at 500dp
-		int requiredWidth = (int) (500 * getResources().getDisplayMetrics().density);
+        // Calculate your sampleSize based on the requiredWidth and
+        // originalWidth
+        // For e.g you want the width to stay consistent at 500dp
+        int requiredWidth = (int) (500 * getResources().getDisplayMetrics().density);
 
-		int sampleSize = (int) Math.ceil((float) imageWidth / (float) requiredWidth);
+        int sampleSize = (int) Math.ceil((float) imageWidth / (float) requiredWidth);
 
-		Log.e("sampleSize ", Integer.toString(sampleSize));
-		// If the original image is smaller than required, don't sample
-		if (sampleSize < 1) {
-			sampleSize = 1;
-		}
+        Log.e("sampleSize ", Integer.toString(sampleSize));
+        // If the original image is smaller than required, don't sample
+        if (sampleSize < 1) {
+            sampleSize = 1;
+        }
 
-		Log.e("sampleSize 2 ", Integer.toString(sampleSize));
-		bmOptions.inSampleSize = sampleSize;
-		bmOptions.inPurgeable = true;
-		bmOptions.inPreferredConfig = Bitmap.Config.RGB_565;
-		bmOptions.inJustDecodeBounds = false;
+        Log.e("sampleSize 2 ", Integer.toString(sampleSize));
+        bmOptions.inSampleSize = sampleSize;
+        bmOptions.inPurgeable = true;
+        bmOptions.inPreferredConfig = Bitmap.Config.RGB_565;
+        bmOptions.inJustDecodeBounds = false;
 
 		/* Decode the JPEG file into a Bitmap */
-		Bitmap bitmap = BitmapFactory.decodeFile(tmpImageFile.getAbsolutePath(), bmOptions);
-				
+        Bitmap bitmap = BitmapFactory.decodeFile(tmpImageFile.getAbsolutePath(), bmOptions);
+        return bitmap;
+    }
+
+	private void setPic() {
+
+        Bitmap bitmap = openImage();
 		if (bitmap == null) {
 			return;
 		}
@@ -323,7 +410,7 @@ public class CameraActivity extends Activity implements PictureCallback, OnClick
 		matrix.setRotate(rotationAngle, (float) bitmap.getWidth() / 2,
 				(float) bitmap.getHeight() / 2);
 		Bitmap rotatedBitmap = Bitmap.createBitmap(bitmap, 0, 0,
-				bmOptions.outWidth, bmOptions.outHeight, matrix, true);
+				bitmap.getWidth(), bitmap.getHeight(), matrix, true);
 
 		/* Associate the Bitmap to the ImageView */
 		mImageView.setImageBitmap(rotatedBitmap);
@@ -355,7 +442,8 @@ public class CameraActivity extends Activity implements PictureCallback, OnClick
             mCurrentPhotoPath = null;
         }
         boolean saved = true;
-        try {				    FileOutputStream fos = new FileOutputStream(pictureFile);
+        try {
+        	FileOutputStream fos = new FileOutputStream(pictureFile);
             fos.write(mCurrentPictureData);
             fos.close();
             galleryAddPic();
